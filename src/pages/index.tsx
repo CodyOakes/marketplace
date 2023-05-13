9aa4266e4a64ebb7c78539d7d65e2e3e1a57a49a
Cody Oakes <codyoakes@me.com>
2023-05-13 05:32:06 -0600

    added navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const user = useUser();
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
@@ -16,14 +14,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user.isSignedIn ? (
-          <UserButton />
-        ) : (
-          <span className="text-white">
-            <SignInButton>Sign In</SignInButton>
-          </span>
-        )}
-
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
